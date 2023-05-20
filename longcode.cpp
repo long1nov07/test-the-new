667968abdbcc8b9abbc9242b63c7ea0849ab7fe1
long1nov07 <124490431+long1nov07@users.noreply.github.com>
2023-05-20 14:01:54 +0700

    Add files via upload

---
+++ longcode.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+//các biến    
+    int SoHocSinh(0);
+    int SoDinhDanh(0);
+    int So0(0);
+//nhập số học sinh
+    cout<<"số học sinh: ";
+    cin>> SoHocSinh;
+    while(So0 < SoHocSinh){
+        ++So0;
+        cout<<"id của học sinh "<< So0 <<":";
+        cin>> SoDinhDanh;
+
+    //lập bảng
+        // cout<< setw(5) << left <<
+
+
+
+    }
+        
+
+
+}
